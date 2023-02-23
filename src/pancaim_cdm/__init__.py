823c63862f66c4e4451346478ab6fe59bcd6bc0e
Hernando Suarez <hernando@Taliana.local>
2023-02-23 11:09:33 +0100

    Adds tables imports to cdm/init

--- src/pancaim_cdm/__init__.py
+++ src/pancaim_cdm/__init__.py
@@ -5,4 +5,11 @@ __version__ = importlib.metadata.version('pancaim-cdm')
 
 from pancaim_cdm.pancaim_orm import (
     Person,
+    BodyMeasurement,
+    Lab,
+    Lab2,
+    Prognosis,
+    Surgery,
+    Therapy,
+    Tumor,
 )
