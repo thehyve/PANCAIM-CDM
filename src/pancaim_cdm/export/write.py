f021ec0b38f9eed5059d5e651b0f41a57599b865
Hernando Suarez <hernando@thehyve.nl>
2023-02-27 15:38:59 +0100

    Adds a bit of documentation

--- src/pancaim_cdm/export/write.py
+++ src/pancaim_cdm/export/write.py
@@ -11,13 +11,8 @@ class CustomJSONEncoder(json.JSONEncoder):
         self.indentation_level = -1
         self.indent = 4
 
-    def encode(self, o) -> str:
-        if isinstance(o, dict):
-            return self._encode_object(o)
-
-        return json.dumps(o)
-
-    def _encode_object(self, o) -> str:
+    def encode(self, o: dict) -> str:
+        """This encodes the initial dictionary, and all non-empty dictionaries"""
         if not o:
             return ''
 
@@ -25,24 +20,26 @@ class CustomJSONEncoder(json.JSONEncoder):
 
         output = []
         for k, v in o.items():
-            if isinstance(v, (str, int, float)) or v is None:
+            if isinstance(v, (str, int, float)) or v is None:                   # to encode key-value pairs in one line (like table fields)
                 output.append(self._encode_item(k, v))
-            elif isinstance(v, dict) and len(v) == 0:
+            elif isinstance(v, dict) and len(v) == 0:                           # to encode empty dictionaries (like empty tables), including "None"
                 output.append(self._encode_empty_dict(k))
-            else:
+            else:                                                               # to encode non-empty dictionaries
                 output.append(
-                    # f"{self.indent_str}{json.dumps(k)}\n{self.encode(v)}"     # keeps basic json format of using double quotes
+                    # f"{self.indent_str}{json.dumps(k)}\n{self.encode(v)}"     # to keeps basic json format of using double quotes, uncomment this and comment line below
                     f"{self.indent_str}{k}\n{self.encode(v)}"
                 )
         self.indentation_level -= 1
         return "\n".join(output)
 
     def _encode_empty_dict(self, k) -> str:
-        # return f"{self.indent_str}{json.dumps(k)}"                            # keeps basic json format of using double quotes
+        """This encodes empty dictionaries (like empty tables)"""
+        # return f"{self.indent_str}{json.dumps(k)}"                            # to keeps basic json format of using double quotes, uncomment this and comment line below
         return f"{self.indent_str}{k}"
 
     def _encode_item(self, k, v) -> str:
-        # return f"{self.indent_str}{json.dumps(k)} = {self.encode(v)}"         # keeps basic json format of using double quotes
+        """This encodes key-value pairs in one line (like table fields), including 'None'"""
+        # return f"{self.indent_str}{json.dumps(k)} = {self.encode(v)}"         # to keeps basic json format of using double quotes, uncomment this and comment line below
         return f"{self.indent_str}{k} = {v}"
 
     def iterencode(self, o, **kwargs) -> str:
